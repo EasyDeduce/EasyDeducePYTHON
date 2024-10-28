debf7dd0044ea22586709d364dc4de10cbf65a8d
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2024-10-28 11:31:01 +0530

    Update DINO_Automator.py

--- DINO_Automator.py
+++ DINO_Automator.py
@@ -1,26 +1,3 @@
-#Install pyautogui and pillow:
-# go to cmd and type-> pip3 install pyautogui
-#                   -> pip3 install pillow
-
-
-
-#creating rectangles:
-#take a screenshot and follow steps below (for 1920 X 820 screen)
-# ##Draw the rectangle for cactus
-        # for i in range(530, 610):
-        #     for j in range(130, 160):
-        #          data[i, j] = 0
-        
-    ## Draw the rectangle for birds
-        # for i in range(530, 560):
-        #     for j in range(100, 125):
-        #         data[i, j] = 171
-
-        # image.show()
-        # break
-
-
-
 #code
 import pyautogui 
 from PIL import Image, ImageGrab 
